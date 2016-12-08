7cd6ff93fa359bd4be8293f4598d8867f8c93905
John Murgo <johnmurgo@gmail.com>
2016-12-08 03:56:52 +0000

    troubleshooting remote failure

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_setting = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'http://warm-tundra-10048.herokuapp.com/',
+  :domain         => 'warm-tundra-10048.herokuapp.com/',
   :authentication => :plain,
 }
 ActionMail::Base.delivery_method = :smtp
