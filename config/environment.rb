f89d38c86f466b0ed7fa501785cfd781547e0480
John Murgo <johnmurgo@gmail.com>
2016-12-08 03:53:08 +0000

    Fixed silly mistake with spaces and marks

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_setting = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV ['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'http://warm-tundra-10048.herokuapp.com/'.
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'http://warm-tundra-10048.herokuapp.com/',
   :authentication => :plain,
 }
 ActionMail::Base.delivery_method = :smtp
